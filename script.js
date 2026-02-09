91a3270e8c9b11a8f6f566a0a57a3dd84e497441
abdurahmaan076 <abdurahmaan0760@gmail.com>
2026-02-09 18:56:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ let menuOpen = false;
 function toggleMenu() {
     const menu = document.getElementById("side-menu");
     if (!menuOpen) {
-        // Gebruik 320px voor een mooie laptop breedte, voelt hetzelfde als telefoon
         menu.style.width = "320px"; 
         menuOpen = true;
     } else {
         menu.style.width = "0";
         menuOpen = false;
-        // Sluit dropdown bij sluiten menu
         document.getElementById("brand-list").style.display = "none";
         document.getElementById("arrow").style.transform = "rotate(0deg)";
     }
